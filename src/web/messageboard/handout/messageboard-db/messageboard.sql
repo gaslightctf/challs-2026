0f0ae11fa2d3eefd5946b8c4b442257204f5462c
sportshead <me@sportshead.dev>
2026-08-14 00:01:22 +0800

    chall(messageboard): fix handout symlink

--- src/web/messageboard/handout/messageboard-db/messageboard.sql
+++ src/web/messageboard/handout/messageboard-db/messageboard.sql
@@ -1,1 +1,12 @@
-../../../messageboard-db/messageboard.sql
+CREATE TABLE IF NOT EXISTS users (
+    name TEXT PRIMARY KEY,
+    secret TEXT NOT NULL,
+
+    public TEXT,
+    public_expiry TIMESTAMPTZ,
+
+    close_friends TEXT,
+    close_friends_expiry TIMESTAMPTZ,
+
+    close_friends_list TEXT[] NOT NULL DEFAULT '{}'
+);
