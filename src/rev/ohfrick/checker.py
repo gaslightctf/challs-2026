2cc5ac18ac2c4d356209c55202e02318b9c91820
sportshead <me@sportshead.dev>
2026-08-14 16:41:22 +0800

    chall(ohfrick): init

---
+++ src/rev/ohfrick/checker.py
@@ -0,0 +1,7 @@
+f,i,o=input("f:"),int,ord
+assert len(f)==14
+T=[f[8]=="_",5**2*2==i(f[1:3]),f[0]==str(True)[-1],"reset"==f[5]+f[0]+f[9]+f[0]+f[3],f[12]+f[i(f[-1])**2]==str(credits).strip()[4:6]]
+F=[i(f[4])+i(f[6])!=i(f[11]),o(f[7])+2!=o(f[0]),i(f[11])%2,str(None)[::-i(f[6])][:2]!=f[0]+f[10],i(f[6])**i(f[i(f[6])])>i(f[-1])]
+assert all(T)
+assert not any(F)
+print("o"+f[-2])
