2f596d5b1ebca35a0f1c4b3706f5303ae59757b8
sportshead <me@sportshead.dev>
2026-08-16 04:40:05 +0800

    chall(good-enough, ucas): yet more cargo culting

--- src/pwn/good-enough/default.nix
+++ src/pwn/good-enough/default.nix
@@ -1,16 +1,36 @@
 {
   dockerTools,
+  runCommand,
   bashInteractive,
   toybox,
   socat,
 }:
+let
+  # one_gadget gives a execve("/bin/sh", NULL, NULL)
+  # use a wrapper to provide a $PATH because we are nice :)
+  shellWrapper = runCommand "shell-wrapper" { } ''
+    mkdir -p $out/bin
+    ln -s ${bashInteractive}/bin/bash $out/bin/bash
+    cat > $out/bin/sh <<'EOF'
+    #!${bashInteractive}/bin/bash
+    export PATH="''${PATH:-/bin}"
+    export HOME="''${HOME:-/}"
+    export TERM="''${TERM:-xterm}"
+    if [ "$#" -eq 0 ]; then
+      exec ${bashInteractive}/bin/bash -i
+    fi
+    exec ${bashInteractive}/bin/bash "$@"
+    EOF
+    chmod +x $out/bin/sh
+  '';
+in
 dockerTools.streamLayeredImage {
   name = "good-enough";
 
   contents = [
     ./handout
 
-    bashInteractive
+    shellWrapper
     toybox
   ];
 
@@ -22,5 +42,7 @@ dockerTools.streamLayeredImage {
   ];
   config.Env = [
     "PATH=/bin"
+    "HOME=/"
+    "TERM=xterm"
   ];
 }
